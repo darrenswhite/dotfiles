a3b25e624f3a519ba380242124efaa5c7df05f57
Darren White <darrenswhite95@gmail.com>
2018-10-20 14:09:35 +0100

    Update awesome wm config

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -239,7 +239,7 @@ awful.screen.connect_for_each_screen(function(s)
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
-            wibox.widget.systray(),
+            -- wibox.widget.systray(),
             battery_widget,
             brightnessctl.widget,
             volumectl.widget,
