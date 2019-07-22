e4e6a2b2d52f3bd3fe7157304150dd5a5f434457
Darren White <darrenswhite95@gmail.com>
2019-07-22 19:30:33 +0100

    Fix bootstrap sync dir

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,6 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master
 
-SYNC_DIR="${1:-~}"
+SYNC_DIR="${1:-${HOME}}"
 
 rsync --exclude ".git/" --exclude "bootstrap.sh" -avh --no-perms . "${SYNC_DIR}";
