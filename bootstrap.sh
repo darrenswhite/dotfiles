35c1c0e369544921083f9b319763d3c3043c0e51
Darren White <darrenswhite95@gmail.com>
2019-07-22 19:20:38 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,4 +6,6 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master
 
-rsync --exclude ".git/" --exclude "bootstrap.sh" -avh --no-perms . ~;
+SYNC_DIR="${1:-~}"
+
+rsync --exclude ".git/" --exclude "bootstrap.sh" -avh --no-perms . "${SYNC_DIR}";
