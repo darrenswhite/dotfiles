a7a9195a32f53f5da08fa18d42ea627c4879b172
dawhite <dawhite@tripadvisor.com>
2024-02-14 11:36:33 +0000

    cleanup

--- .screenlayout/external.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP1 --primary --auto --below DP1 --output DP1 --auto --output DP2 --off
--- .screenlayout/internal.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP1 --primary --mode auto --output DP1 --off --output DP2 --off
