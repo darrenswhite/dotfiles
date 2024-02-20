e748f66d487dcd70cf9a763936a21834d1f555a3
dawhite <dawhite@tripadvisor.com>
2024-02-20 13:41:45 +0000

    Update neovim commands

--- neovim/.config/nvim/lua/config/commands.lua
+++ neovim/.config/nvim/lua/config/commands.lua
@@ -3,8 +3,8 @@ vim.keymap.set('n', '<leader>r', function()
     local file = vim.fn.expand('%') -- Get the current file name
     local first_line = vim.fn.getline(1) -- Get the first line of the file
     if string.match(first_line, '^#!/') then -- If first line contains shebang
-        local escaped_file = vim.fn.shellescape(file) -- Properly escape the file name for shell commands
-        vim.cmd('!chmod +x ' .. escaped_file) -- Make the file executable
+        local escaped_file = vim.fn.getcwd() .. '/' .. vim.fn.shellescape(file) -- Properly escape the file name for shell commands
+        vim.cmd('!chmod u+x ' .. escaped_file) -- Make the file executable
         vim.cmd('vsplit') -- Split the window vertically
         vim.cmd('terminal ' .. escaped_file) -- Open terminal and execute the file
         vim.cmd('startinsert') -- Enter insert mode, recommended by echasnovski on Reddit
